0f321a28eeaa6c58e521d5b91c4f1a8b692d6361
Karnisha Karunakaran <karnisha.k@itp.objectfrontier.com>
2024-05-04 10:14:35 +0530

    Initial

---
+++ LXP/LXP.Data/DBContexts/LXPDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using LXP.Data;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
+
+namespace LXP.Data.DBContexts;
+
+public partial class LXPDbContext : DbContext
+{
+    public LXPDbContext()
+    {
+    }
+
+    public LXPDbContext(DbContextOptions<LXPDbContext> options)
+        : base(options)
+    {
+    }
+
+}
